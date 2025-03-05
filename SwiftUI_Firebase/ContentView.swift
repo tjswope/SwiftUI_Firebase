9e94c5acab8fc9e7db6395214c00d3c18d8c5f74
Tom Swope <tjswope@gmail.com>
2025-03-05 10:16:51 -0500

    Initial Commit

---
+++ SwiftUI_Firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Firebase
+//
+//  Created by Swope, Thomas on 3/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Firebase/SwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FirebaseApp.swift
+//  SwiftUI_Firebase
+//
+//  Created by Swope, Thomas on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
